3b4fa1a230a11d642462501d2dab9ae1a1815bbf
Jonathan Viegut <viegutj@gmail.com>
2023-05-27 11:20:07 -0500

    Made more updates to stretch goals. Got stuck; will return to try again.

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -87,7 +87,12 @@ console.log('Test b - should say true', find(1, [1,2,3,4]));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  console.log("This is a the first letter in the string:");
+  if (isFirstLetter.unshift == (letter)) {
+    return true
+  } else {
+    return false
+  }
 }
 console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
 console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
@@ -96,16 +101,26 @@ console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
 function sumAll() {
   let sum = 0;
   // TODO: loop to add items
-
+  for (let i = 0; i < sumAll.length; index++) {
+    sum += sumAll[i];
+    return console.log(sum);
+  }
   // TODO: return the sum
+  return console.log(sum);
 }
+
+console.log(sumAll([1,2,3,4]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-function allPositive() {
-
-}
+// function allPositive() {
+//   if (i > 0) {
+//     return console.log([i]);
+//   } else (i <= 0)
+//     return console.log('no positive numbers');
+// }
+// console.log(allPositive());
 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
